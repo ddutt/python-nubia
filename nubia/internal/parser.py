4b79a36feed483e7f672194ed711cc6d004a91ce
Dinesh Dutt <dd.ps4u@gmail.com>
2020-04-26 23:45:40 -0700

    move recognition of float around to handle IP addr

--- nubia/internal/parser.py
+++ nubia/internal/parser.py
@@ -68,7 +68,7 @@ unquoted_string = pp.Word(
 
 string_value = quoted_string | unquoted_string
 
-single_value = bool_value | float_value | string_value | int_value
+single_value = bool_value | string_value | float_value | int_value
 
 list_value = pp.Group(
     pp.Suppress("[")
